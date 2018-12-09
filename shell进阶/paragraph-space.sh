27d08164dadb1978594953099020353b71e408c3
hiro <liuhong@dianwoba.com>
2018-12-09 14:15:38 +0800

    文本插入空行小练习

--- shell进阶/paragraph-space.sh
+++ shell进阶/paragraph-space.sh
@@ -3,3 +3,22 @@
 # @Mail    : hiroliu@yeah.net
 # @Time    : 2018/12/6 7:33 PM
 
+# 在无空行的文本文件的段落之间插入空行
+# 像这样使用：$0 <FILENAME
+
+MINLEN=60  # 可以试试修改这个值。它用来做判断。
+# 假设一行的字符数小鱼 $MINLEN，并且以句点结束段落。
+
+echo $2
+
+while read -r line || [[ -n $line ]] # 当文件有很多行的时候
+do
+    echo "$line"  # 输出行本身
+    len="${#line}"
+    if [[ "${len}" -lt ${MINLEN} && "${line}" =~ [*{\.!?}]$ ]]
+    then
+        echo  # 在该行 以句点，问号，感叹号结束时，增加一行空行
+    fi
+done
+
+exit
